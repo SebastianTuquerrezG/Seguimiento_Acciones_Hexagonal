536594756c089cdf7a436650a74d48cd1877a3c4
DanielSolarte3 <danielsolarte1016@gmail.com>
2023-12-10 17:05:05 -0500

    Creacion de proyecto

---
+++ SeguimientoAcciones/src/main/java/co/unicauca/SeguimientoAcciones/SeguimientoAccionesApplication.java
@@ -0,0 +1,13 @@
+package co.unicauca.SeguimientoAcciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguimientoAccionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguimientoAccionesApplication.class, args);
+	}
+
+}
---
+++ SeguimientoAcciones/src/test/java/co/unicauca/SeguimientoAcciones/SeguimientoAccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.SeguimientoAcciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguimientoAccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
